812f68c1887a1e008c417909eeec3ec3b6b6c5ae
doanvanngoi84vl-coder <doanvanngoi84vl@gmail.com>
2025-10-05 17:00:51 +0700

    Add files via upload

--- flood.js
+++ flood.js
@@ -214,12 +214,13 @@ language_header = [
           "ecdsa_secp256r1_sha256",
         "ecdsa_secp384r1_sha384",
         "ecdsa_secp256r1_sha256:rsa_pss_rsae_sha256:rsa_pkcs1_sha256:ecdsa_secp384r1_sha384:rsa_pss_rsae_sha384:rsa_pkcs1_sha384:rsa_pss_rsae_sha512:rsa_pkcs1_sha512",
-         , 'dsa_sha256',
+          "dsa_sha256",
 	"dsa_sha384",
 	"dsa_sha512",
 	"dsa_sha224", 
 	"dsa_sha1",
 ]
+
   let SignalsList = sigalgs.join(':')
 const ecdhCurve = "GREASE:X25519:x25519:P-256:P-384:P-521:X448";
 const secureOptions =
